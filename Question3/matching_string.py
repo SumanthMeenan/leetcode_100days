80823dd5a9871b0ea5146edaac80b30ff7578622
Sumanth Meenan <skanneti1@student.gsu.edu>
2022-10-10 19:15:55 -0400

    matching strings

---
+++ Question3/matching_string.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_str, max_str = min(strs), max(strs)
+        for index, val in enumerate(min_str):
+            print(index, val)
+            if val != max_str[index]:
+                return min_str[:index]
+        return min_str 
+
+     
+if __name__ == "__main__":
+    strs = input("List of strings: ")
+    print(Solution().longestCommonPrefix(strs))
